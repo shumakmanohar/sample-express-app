591a9580174b277da155a65131caa6af02f42a8b
shumakmanohar <shumakmanohar123@gmail.com>
2023-03-16 13:54:01 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+	res.send("<h1>Hello From Express App</h1>");
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
